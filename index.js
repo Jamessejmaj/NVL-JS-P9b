484eac72b9e1129e2b22b5fdff77c25876400e38
Jamessejmaj <56188968+Jamessejmaj@users.noreply.github.com>
2019-10-29 11:21:38 +0100

    ejericio

---
+++ index.js
@@ -0,0 +1,4 @@
+var a = "Lunes, Martes, Miércoles, Jueves , Viernes, Sábado, Domingo" ;
+var b = "Madrid,Sevilla,Barcelona,Valencia,Zaragoza,Bilbao,Donosti " ;
+var array= [[Lunes,Madrid],[Martes, Sevilla],[Miércoles,Barcelona],[Jueves, Valencia],[Viernes,Zaragoza],[Sábado, Bilbao],[Domingo,Donosti]] ;
+console.log (matriz [0] [1] [2] [3] [4] [5] [6]) ;
